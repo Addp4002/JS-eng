d4f9826393f627607ca8f05f00ab1cdcb692c4b8
Arghya Dandapat <137853606+Addp4002@users.noreply.github.com>
2026-02-10 16:59:52 +0000

    done

--- .devcontainer/01_basics/01_variable.js
+++ .devcontainer/01_basics/01_variable.js
@@ -2,5 +2,11 @@ const accountId = 144553
 let accountEmail = "arghya@gmail.com"
 var accountPassword = "12345"
 let accountCity = "Jaipur"
+accountEmail="ad@gmail.com"
+accountPassword="43256"
+accountCity="kolkata"
 
+//prefer not to use var,
+//because of issuein block scope and funcctional scope,
 console.log(accountId);
+console.table([accountId,accountCity,accountEmail,accountPassword])
