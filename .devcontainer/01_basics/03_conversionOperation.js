815777768147b09386f81e4b61be28f1945ba20a
Arghya Dandapat <137853606+Addp4002@users.noreply.github.com>
2026-02-10 17:37:17 +0000

    done

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = 33 // if "33" then it will be string
+console.log(typeof score);
+let valuIneNumber = Number(score)
+console.log(typeof valuIneNumber );
+console.log( valuIneNumber );
+//"33abc" : NaN
+//true : 1; false : 0
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 : true; 0 : false
+//" " false
+// "Arghya" : true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
