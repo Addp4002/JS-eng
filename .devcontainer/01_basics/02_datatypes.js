e19db6f262a6dced2cd4dd74e0dd2b444571c024
Arghya Dandapat <137853606+Addp4002@users.noreply.github.com>
2026-02-10 17:15:41 +0000

    done

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) we are using Node js,not browser
+console.log("Arghya")
+let name="Arghya" //string : ""
+let age = 21 //number  : 2 to power 53 //bigint
+let isLogged=false //boolean ; T or F
+let state ; //null : stand alone value
+//undefined : 
+//symbol : unique
+//object
+
+
+console.log(typeof "Arghya"); // identify type : string or no or else
