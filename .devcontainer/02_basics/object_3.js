1affd6f2e0dd909e0dbcea1dd2884410a666be21
Arghya Dandapat <137853606+Addp4002@users.noreply.github.com>
2026-02-15 09:58:11 +0000

    done

---
+++ .devcontainer/02_basics/object_3.js
@@ -0,0 +1,28 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+ //course.courseInstructor
+
+//const {courseInstructor} = course
+//console.log(courseInstructor);
+
+//destructure
+//const {courseInstructor: Instructor} = course
+console.log(Instructor);
+
+//API ------> JSON
+
+ /*{
+     "name": "hitesh",
+     "coursename": "js in hindi",
+     "price": "free"
+}*/
+[
+    {},
+    {},
+    {}
+]
+
